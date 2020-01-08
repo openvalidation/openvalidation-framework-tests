46780631e3d4dc6a60fa6505d9f053842b157391
Lionel Pereira Azevedo <lazevedo@brockhaus-ag.de>
2020-01-08 14:14:18 +0100

    merge

--- OpenValidationFramework-Java/src/main/java/org/bag/model/primitives/IntModel.java
+++ OpenValidationFramework-Java/src/main/java/org/bag/model/primitives/IntModel.java
@@ -1,8 +1,12 @@
 package org.bag.model.primitives;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntModel {
     private int[] ints;
     private int value;
+    private List<Integer> intList = new ArrayList<>();
 
     public int[] getInts() {
         return ints;
@@ -18,5 +22,13 @@ public class IntModel {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    public List<Integer> getIntList() {
+        return intList;
+    }
+
+    public void setIntList(List<Integer> intList) {
+        this.intList = intList;
     }
 }
